d8c12650a5f7550908136ef678825057f95a092e
chingusdeveloper@gmail.com <chingusdevleoper@gmail.com>
2016-03-07 16:39:34 +0530

    login form

--- src/main/java/com/test/project/controller/TestProjectController.java
+++ src/main/java/com/test/project/controller/TestProjectController.java
@@ -1,8 +1,7 @@
 package com.test.project.controller;
  
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,17 +9,39 @@ import org.springframework.web.servlet.ModelAndView;
  
 @Controller
 public class TestProjectController {
+	
+	private static final Logger logger = Logger.getLogger(TestProjectController.class);
+
 	String message = "Welcome to Spring MVC!";
 	String message2 = "Welcome to Spring MVC 2!";
  
 	@RequestMapping("/hello")
 	public ModelAndView showMessage(
-			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+			@RequestParam(value = "username", required=false ) String user_Name, 	@RequestParam(value = "password", required=false) String password) {
+		
+		
 		System.out.println("in controller");
- 
-		ModelAndView mv = new ModelAndView("helloworld");
+		System.out.println("Logged in User:: " + user_Name + " " + "Password:: " + password);
+		
+		ModelAndView mv;
+		if(logger.isDebugEnabled()){
+			logger.debug("showMessage is executed!");
+		}
+		
+		logger.info("in controller");
+		logger.info("Logged in User:: " + user_Name + " " + "Password:: " + password);
+		
+		if(!user_Name.equalsIgnoreCase("admin") || !password.equalsIgnoreCase("admin")){
+			  mv = new ModelAndView("logIn");
+			  mv.addObject("msg", "Invalid Credentials!!");
+				
+		}
+		else{
+	    mv = new ModelAndView("helloworld");
 		mv.addObject("message", message);
-		mv.addObject("name", name);
+		mv.addObject("name", user_Name);
+		}
+		
 		return mv;
 	}
 	
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,19 +1,38 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
- 
-pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 <title>Spring 4 MVC - HelloWorld Index Page</title>
 </head>
-<body>
- 
-	<center>
-		<h2>Hello World Test Project</h2>
-		<h3>
-			<a href="hello?name=omkar">Click Here</a>
-		</h3>
-	</center>
+<body bgcolor="#FFFFC2">
+<center>
+<h3>Log In Form</h3>
+	<form name='loginForm' id="loginForm" action="hello" method='POST' >
+
+		<table border="1" bgcolor="#FF8040">
+			<tr>
+				<td>User:</td>
+				<td><input type='text' name='username' value=''></td>
+			</tr>
+			<tr>
+				<td>Password:</td>
+				<td><input type='password' name='password' /></td>
+			</tr>
+			<tr align="right">
+				<td align="center" colspan="2"><input align="top" name="submit" type="submit" value="submit" onclick="submitLogin()" />
+					<input align="top" name="reset" type="reset" /></td>
+			</tr>
+		</table>
+
+	</form>
+</center>
+
+	<script>
+		function submitLogin() {
+			document.getElementById(loginForm).submit();
+		}
+	</script>
 </body>
 </html>
